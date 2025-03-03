e4423ba7fe9246576c53ccb5300ff59d5d2b8b43
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-03-03 19:36:54 +0100

    Add Style
    Change name to GeminiPostSQL REST API
    Added some Style

--- AIAPI/Program.cs
+++ AIAPI/Program.cs
@@ -9,11 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "AI REST API",
-        Version = "v1"
-    });
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "GeminiPostSQL REST API", Version = "v1", Description = "REST API with AI-Assisted Endpoints" });
 
     c.EnableAnnotations();
 });
@@ -24,8 +20,20 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options => {
-        options.DisplayRequestDuration();
+    app.UseSwaggerUI(options =>
+    {   // Aditional options
+        options.DefaultModelsExpandDepth(-1); // Removes the model list at the end of the page
+        // Add the documents to the dropdown
+        options.DocumentTitle = "GeminiPostSQL API"; // Browser Tab Name
+        options.EnableFilter(); // Creates an Text box to filter Controllers
+        options.DisplayRequestDuration(); // Shows the runtime ms on every Endpoint
+        // Deafault state of the Endpoints (Expanded or Hiden)
+        //options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.Full); 
+        //options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+        // Header Custom content
+        options.HeadContent = "<link rel=\"icon\" type=\"image/png\" href=\"/Img/Favicon.png\">" // Favicon (wwwroot/Img/...)
+                            + "<span class=\"header\">&emsp;Generated with Swashbuckle's Swagger UI</span>"; // Custom Header Text
+        options.InjectStylesheet("/Style/style.css"); // Custom CSS (wwwroot/Style/...)
     });
 }
 
